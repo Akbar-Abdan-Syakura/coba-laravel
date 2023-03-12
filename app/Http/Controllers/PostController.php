b4e2179de1ad573e86903431a89b900ea67810a5
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2023-03-13 03:17:23 +0700

    belum selesai vid 13, lanjut di pc

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use PhpParser\Node\Stmt\If_;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::latest();
+
+        if (request('search')) {
+            $posts->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+
         return view('posts', [
             "title" => "All Posts",
-            // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "posts" => $posts->get(),
         ]);
     }
 
